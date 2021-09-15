b526d33f7f8a51f9b1c6a754a3dc887a70614dc6
ferdousanam <ferdous.anam@gmail.com>
2021-09-15 20:44:50 +0600

    JS Bundled

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
+    <meta name="description" content="Inertia JS with Laravel and VueJS 2">
     <meta name="author" content="Ferdous Anam">
     <meta name="keyword" content="FlatLab, Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
@@ -21,28 +21,7 @@
 
 @inertia
 
-<!-- js placed at the end of the document so the pages load faster -->
-<script src="http://thevectorlab.net/flatlab-4/js/jquery.js"></script>
-<script src="http://thevectorlab.net/flatlab-4/js/bootstrap.bundle.min.js"></script>
-<script class="include" type="text/javascript" src="http://thevectorlab.net/flatlab-4/js/jquery.dcjqaccordion.2.7.js"></script>
-<script src="http://thevectorlab.net/flatlab-4/js/jquery.scrollTo.min.js"></script>
-<script src="http://thevectorlab.net/flatlab-4/js/jquery.nicescroll.js" type="text/javascript"></script>
-<script src="http://thevectorlab.net/flatlab-4/js/jquery.sparkline.js" type="text/javascript"></script>
-<script src="http://thevectorlab.net/flatlab-4/assets/jquery-easy-pie-chart/jquery.easy-pie-chart.js"></script>
-<script src="http://thevectorlab.net/flatlab-4/js/owl.carousel.js" ></script>
-<script src="http://thevectorlab.net/flatlab-4/js/jquery.customSelect.min.js" ></script>
-<script src="http://thevectorlab.net/flatlab-4/js/respond.min.js" ></script>
-
-<!--right slidebar-->
-<script src="http://thevectorlab.net/flatlab-4/js/slidebars.min.js"></script>
-
-<!--common script for all pages-->
-<script src="http://thevectorlab.net/flatlab-4/js/common-scripts.js"></script>
-
-<!--script for this page-->
-<script src="http://thevectorlab.net/flatlab-4/js/sparkline-chart.js"></script>
-<script src="http://thevectorlab.net/flatlab-4/js/easy-pie-chart.js"></script>
-<script src="http://thevectorlab.net/flatlab-4/js/count.js"></script>
+<script src="{{ mix('/js/vendors.js') }}"></script>
 <script src="{{ mix('/js/app.js') }}" defer></script>
 </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -15,6 +15,18 @@ const path = require('path');
 mix.js('resources/js/app.js', 'public/js')
     .vue()
     .sass('resources/scss/app.scss', 'public/css')
+    .scripts([
+        'node_modules/popper.js/dist/umd/popper.js',
+        'node_modules/jquery/dist/jquery.js',
+        'resources/js/libs/jquery.dcjqaccordion.2.7.js',
+        'node_modules/bootstrap/dist/js/bootstrap.bundle.js',
+        'node_modules/jquery.scrollto/jquery.scrollTo.js',
+        'node_modules/jquery.nicescroll/dist/jquery.nicescroll.js',
+        'node_modules/jquery-sparkline/jquery.sparkline.js',
+        'node_modules/respond.js/dest/respond.min.js',
+        'resources/js/libs/slidebars.min.js',
+        'resources/js/common-scripts.js',
+    ], 'public/js/vendors.js')
     .webpackConfig({
         output: {chunkFilename: 'js/[name].js?id=[chunkhash]'},
         resolve: {
