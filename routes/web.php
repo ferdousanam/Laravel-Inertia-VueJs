11004af4c7bcccb71612598e08871714ca70be91
ferdousanam <ferdous.anam@gmail.com>
2021-09-15 23:17:34 +0600

    Laravel UI Installation

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,5 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', DashboardController::class);
-Route::resource('/users', UserController::class);
+Auth::routes();
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', DashboardController::class);
+    Route::resource('/users', UserController::class);
+});
