117c29ed4a824d79d932e32d29f03864907bf9cd
ferdousanam <ferdous.anam@gmail.com>
2021-09-22 17:10:25 +0600

    User details page updated

--- resources/js/Pages/User/Show.vue
+++ resources/js/Pages/User/Show.vue
@@ -1,28 +1,37 @@
 <template>
-    <div>
-        <div class="box-body table-responsive">
-            <table class="table table-bordered">
-                <caption class="hidden"><h3>User Details</h3></caption>
-                <thead>
-                <tr class="hidden">
-                    <th style="width:230px;"></th>
-                    <th style="width:10px;"></th>
-                    <td></td>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <th style="width:230px;">Full Name</th>
-                    <th style="width:10px;">:</th>
-                    <td>{{ user.name }}</td>
-                </tr>
-                <tr>
-                    <th>Email Address</th>
-                    <th>:</th>
-                    <td>{{ user.email }}</td>
-                </tr>
-                </tbody>
-            </table>
+    <div class="row">
+        <div class="col-sm-12">
+            <section class="card">
+                <header class="card-header">
+                    User Details
+                </header>
+                <div class="card-body">
+                    <div class="box-body table-responsive">
+                        <table class="table table-bordered">
+                            <caption class="hidden"><h3>User Details</h3></caption>
+                            <thead>
+                            <tr class="hidden">
+                                <th style="width:230px;"></th>
+                                <th style="width:10px;"></th>
+                                <td></td>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <th style="width:230px;">Full Name</th>
+                                <th style="width:10px;">:</th>
+                                <td>{{ user.name }}</td>
+                            </tr>
+                            <tr>
+                                <th>Email Address</th>
+                                <th>:</th>
+                                <td>{{ user.email }}</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </section>
         </div>
     </div>
 </template>
