cbb597e3f523a38504a997fffcf781eefceebe3a
ferdousanam <ferdous.anam@gmail.com>
2021-09-22 17:11:36 +0600

    User list page updated

--- resources/js/Pages/User/Index.vue
+++ resources/js/Pages/User/Index.vue
@@ -6,28 +6,30 @@
                     User List
                     <Link class="btn btn-primary pull-right" :href="'/users/create'"><i class="fa fa-plus-square-o"></i> Add User</Link>
                 </header>
-                <table class="table table-striped">
-                    <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Full Name</th>
-                        <th>Email Address</th>
-                        <th>Actions</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr v-for="(user, index) in users.data">
-                        <td class="align-middle">{{ index + 1 }}</td>
-                        <td class="align-middle">{{ user.name }}</td>
-                        <td class="align-middle">{{ user.email }}</td>
-                        <td class="align-middle">
-                            <Link :href="'/users/' + user.id" class="btn btn-primary btn-sm">Show</Link>
-                            <Link :href="'/users/' + user.id + '/edit'" class="btn btn-secondary btn-sm">Edit</Link>
-                            <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteModal" @click="deletable_id = user.id">Delete</button>
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
+                <div class="card-body">
+                    <table class="table table-striped">
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Full Name</th>
+                            <th>Email Address</th>
+                            <th>Actions</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr v-for="(user, index) in users.data">
+                            <td class="align-middle">{{ index + 1 }}</td>
+                            <td class="align-middle">{{ user.name }}</td>
+                            <td class="align-middle">{{ user.email }}</td>
+                            <td class="align-middle">
+                                <Link :href="'/users/' + user.id" class="btn btn-primary btn-sm">Show</Link>
+                                <Link :href="'/users/' + user.id + '/edit'" class="btn btn-secondary btn-sm">Edit</Link>
+                                <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteModal" @click="deletable_id = user.id">Delete</button>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
                 <footer class="card-footer">
                     <div class="px-5">
                         <pagination class="float-right" :links="users.links"/>
